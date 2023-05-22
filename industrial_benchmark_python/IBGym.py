a2b374f7839566bdbd05028211f251abac0dbe1e
Tsesterh <77850788+Tsesterh@users.noreply.github.com>
2023-05-22 10:53:06 +0200

    Update IBGym.py with gymnasium

--- industrial_benchmark_python/IBGym.py
+++ industrial_benchmark_python/IBGym.py
@@ -24,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-import gym
+import gymnasium as gym
 import numpy as np
 from collections import OrderedDict
 
